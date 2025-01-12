8081ad9b593cfd2f333cfdcbb9bf0d225224cec6
CDunkel1 <110947489+CDunkel1@users.noreply.github.com>
2025-01-12 17:17:48 -0500

    Update index.js
    update path

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { tweetsData } from '/Users/cheralynciesielski/Desktop/projects/twitter_clone/data.js'
+import { tweetsData } from '/twitter_clone/data.js'
 import { v4 as uuidv4 } from 'https://jspm.dev/uuid';
 
 /*
